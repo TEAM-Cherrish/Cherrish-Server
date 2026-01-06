4ce71ce1896fbad0d1938fab032f12a9fc2f28e5
ssyoung02 <bera1125@naver.com>
2026-01-07 01:46:11 +0900

    feat (UserProcedure): 사용자 시술 일정 등록 관련 DTO 구현

---
+++ src/main/java/com/sopt/cherrish/domain/userprocedure/presentation/dto/request/UserProcedureCreateRequestDto.java
@@ -0,0 +1,39 @@
+package com.sopt.cherrish.domain.userprocedure.presentation.dto.request;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(description = "사용자 시술 일정 등록 요청")
+public class UserProcedureCreateRequestDto {
+
+	@Schema(description = "예약 날짜 및 시간", example = "2025-01-01T16:00:00", requiredMode = Schema.RequiredMode.REQUIRED)
+	@NotNull(message = "예약 날짜 및 시간은 필수입니다")
+	private LocalDateTime scheduledAt;
+
+	@Schema(description = "시술 목록", requiredMode = Schema.RequiredMode.REQUIRED)
+	@NotEmpty(message = "시술 목록은 비어 있을 수 없습니다")
+	@Valid
+	private List<UserProcedureCreateRequestItemDto> procedures;
+
+	@JsonCreator
+	public UserProcedureCreateRequestDto(
+		@JsonProperty("scheduledAt") LocalDateTime scheduledAt,
+		@JsonProperty("procedures") List<UserProcedureCreateRequestItemDto> procedures
+	) {
+		this.scheduledAt = scheduledAt;
+		this.procedures = procedures;
+	}
+}
