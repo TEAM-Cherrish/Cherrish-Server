adb1f16613251ef5ab0ede99414079f5c1594357
Kimgyuilli <rlarbdlf222@gmail.com>
2026-01-12 01:24:40 +0900

    test(challenge): ChallengeSchedulerServiceTest 오류 수정

--- src/test/java/com/sopt/cherrish/domain/challenge/core/application/scheduler/ChallengeSchedulerServiceTest.java
+++ src/test/java/com/sopt/cherrish/domain/challenge/core/application/scheduler/ChallengeSchedulerServiceTest.java
@@ -8,7 +8,6 @@ import static org.mockito.Mockito.when;
 
 import java.time.LocalDate;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
