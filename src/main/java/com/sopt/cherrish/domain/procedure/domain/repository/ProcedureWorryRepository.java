dd30d6d066b4eeb8104e2787613b8088e6d63d94
ssyoung02 <bera1125@naver.com>
2026-01-13 01:26:06 +0900

    feat (Procedure): 시술-피부고민 매핑 레포지토리 추가

---
+++ src/main/java/com/sopt/cherrish/domain/procedure/domain/repository/ProcedureWorryRepository.java
@@ -0,0 +1,20 @@
+package com.sopt.cherrish.domain.procedure.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.sopt.cherrish.domain.procedure.domain.model.ProcedureWorry;
+
+public interface ProcedureWorryRepository extends JpaRepository<ProcedureWorry, Long> {
+
+	@Query("""
+		select pw
+		from ProcedureWorry pw
+		join fetch pw.worry
+		where pw.procedure.id in :procedureIds
+		""")
+	List<ProcedureWorry> findAllByProcedureIdInWithWorry(@Param("procedureIds") List<Long> procedureIds);
+}
