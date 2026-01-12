7ca9c43799b276099a25ba58107ddef8eaf2697e
ssyoung02 <bera1125@naver.com>
2026-01-12 22:57:53 +0900

    refactor (Procedure): Repository 레벨에서 고민 fetch 로직 이동/정리

--- src/main/java/com/sopt/cherrish/domain/procedure/domain/repository/ProcedureRepositoryCustom.java
+++ src/main/java/com/sopt/cherrish/domain/procedure/domain/repository/ProcedureRepositoryCustom.java
@@ -3,6 +3,7 @@ package com.sopt.cherrish.domain.procedure.domain.repository;
 import java.util.List;
 
 import com.sopt.cherrish.domain.procedure.domain.model.Procedure;
+import com.sopt.cherrish.domain.procedure.domain.model.ProcedureWorry;
 
 public interface ProcedureRepositoryCustom {
 
@@ -13,4 +14,10 @@ public interface ProcedureRepositoryCustom {
 	 * - DISTINCT로 중복 제거 (한 시술이 여러 고민과 매핑되는 경우)
 	 */
 	List<Procedure> searchProcedures(String keyword, Long worryId);
+
+	/**
+	 * 시술 ID 목록으로 ProcedureWorry 조회 (Worry fetch join)
+	 * - N+1 문제 방지를 위해 Worry를 한 번에 가져옴
+	 */
+	List<ProcedureWorry> findAllByProcedureIdInWithWorry(List<Long> procedureIds);
 }
--- src/main/java/com/sopt/cherrish/domain/procedure/domain/repository/ProcedureWorryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sopt.cherrish.domain.procedure.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sopt.cherrish.domain.procedure.domain.model.ProcedureWorry;
-
-public interface ProcedureWorryRepository extends JpaRepository<ProcedureWorry, Long> {
-}
