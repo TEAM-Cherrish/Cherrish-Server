588c9df113c763d5927b6517a13a271547b22f11
ssyoung02 <bera1125@naver.com>
2026-01-13 15:45:53 +0900

    feat (Calendar): 시술별 다운타임 조회를 위한 쿼리 및 에러코드 추가

--- src/main/java/com/sopt/cherrish/domain/userprocedure/domain/repository/UserProcedureRepository.java
+++ src/main/java/com/sopt/cherrish/domain/userprocedure/domain/repository/UserProcedureRepository.java
@@ -1,9 +1,19 @@
 package com.sopt.cherrish.domain.userprocedure.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.sopt.cherrish.domain.userprocedure.domain.model.UserProcedure;
 
 public interface UserProcedureRepository extends JpaRepository<UserProcedure, Long>,
 	UserProcedureRepositoryCustom {
+
+	@Query("SELECT up FROM UserProcedure up JOIN FETCH up.procedure WHERE up.id = :userProcedureId AND up.user.id = :userId")
+	Optional<UserProcedure> findByIdAndUserIdWithProcedure(
+		@Param("userProcedureId") Long userProcedureId,
+		@Param("userId") Long userId
+	);
 }
