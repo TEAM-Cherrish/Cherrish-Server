fef823a170c5e1ef1d47ddea3f06daa94e7fb220
Kimgyuilli <rlarbdlf222@gmail.com>
2026-01-21 04:59:04 +0900

    feat(ErrorCode): 유효하지 않은 method ErrorCode 추가

--- src/main/java/com/sopt/cherrish/global/response/error/ErrorCode.java
+++ src/main/java/com/sopt/cherrish/global/response/error/ErrorCode.java
@@ -6,6 +6,7 @@ public enum ErrorCode implements ErrorType {
 	INVALID_FORMAT("C002", "데이터 형식이 올바르지 않습니다", 400),
 	CONCURRENT_UPDATE("C003", "다른 사용자가 동시에 수정했습니다. 다시 시도해주세요.", 409),
 	NOT_FOUND("C004", "요청한 리소스를 찾을 수 없습니다", 404),
+	METHOD_NOT_ALLOWED("C005", "지원하지 않는 HTTP 메서드입니다", 405),
 
 	INTERNAL_SERVER_ERROR("C999", "서버 내부 오류가 발생했습니다", 500);
 
