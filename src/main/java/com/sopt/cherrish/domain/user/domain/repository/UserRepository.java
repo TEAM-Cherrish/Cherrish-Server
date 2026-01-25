ca2796dcdb480c346dd2dfd71fb69db32d477101
Kimgyuilli <rlarbdlf222@gmail.com>
2026-01-25 21:07:53 +0900

    feat(global): redis config 추가

--- src/main/java/com/sopt/cherrish/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/sopt/cherrish/domain/user/domain/repository/UserRepository.java
@@ -1,8 +1,15 @@
 package com.sopt.cherrish.domain.user.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.sopt.cherrish.domain.auth.domain.model.SocialProvider;
 import com.sopt.cherrish.domain.user.domain.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findBySocialProviderAndSocialId(SocialProvider socialProvider, String socialId);
+
+	boolean existsBySocialProviderAndSocialId(SocialProvider socialProvider, String socialId);
 }
